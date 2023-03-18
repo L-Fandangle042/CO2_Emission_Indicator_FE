0c5e760387ffe0908f4f4f46613dd66d130015dc
Lloyd McKinney <42.lloyd.mckinney@gmail.com>
2023-03-18 12:53:37 +0000

    Updated Streamlit api

--- app.py
+++ app.py
@@ -2,51 +2,49 @@ import streamlit as st
 import requests
 import pandas as pd
 import plotly.express as px
+import json
+
+
 
 # @st.experimental_memo
-CO2_yearly_path = "/Users/anna/code/L-Fandangle042/CO2_Emission_Indicator/data/carbon_dioxide/CO2_simplified.xlsx"
+CO2_yearly_path = "/Users/anna/code/L-Fandangle042/CO2_Emission_Indicator/data/carbon_dioxide/CO2_simplified_by_name.xlsx"
 df = pd.read_excel(CO2_yearly_path)
 
 # @st.experimental_memo
-CO2_region_path = "/Users/anna/code/L-Fandangle042/CO2_Emission_Indicator/data/carbon_dioxide/CO2_region.xlsx"
-df_region = pd.read_excel(CO2_region_path)
+# CO2_region_path = "/Users/anna/code/L-Fandangle042/CO2_Emission_Indicator/data/carbon_dioxide/CO2_region.xlsx"
+# df_region = pd.read_excel(CO2_region_path)
 
-st.header("CO2 Emission Indicator")
+st.header("Welcome to the CO2 Emissions predictor")
+st.text("Select a country on the sidebar and click 'Predict' 🚀")
 
+# Country filter
+st.sidebar.title("Filters")
 countries_list = df['country'].unique()
-select_country = st.sidebar.multiselect("Select Country", options=countries_list) #, default=countries_list)
+country_selected = st.sidebar.selectbox("Select country", options=countries_list) #, default=countries_list)
+df_selection = df.query("country == @country_selected")
 
-df_selection = df.query("country == @select_country")
-# df_selected = df[df['country'] == select_country]
+# Graph
+if country_selected:
+    st.write('Country Selected:', country_selected)
 
-fig = px.line(df_selection, x="year", y="CO2", color='country')
-st.plotly_chart(fig)
-
-
-
-# df_selected = df[df['country'] == select_country]
-
-# if select_country:
-#     fig2 = px.line(df_selected, x="year", y="CO2")
-#     st.plotly_chart(fig2)
-# else:
-#     st.plotly_chart(fig)
-
-# st.write('Country Selected:', select_country)
+fig = px.line(df_selection, x="year", y="CO2", color='country',
+              title='CO2 emissions by country and year')
+st.plotly_chart(fig, use_container_width=True)
 
 
+# API implementation
+url = 'http://127.0.0.1:8000/predict'
+
+if st.sidebar.button("Predict"):
+    # params = {"country": "Aruba", "max_predicted_year": 2030}
+    # response = requests.post(url, params=params)
+    # st.subheader(response)
+    # st.subheader(response.text)
+    # prediction = response.json()['prediction']
+
+    # question = f'Will {country_selected[0]} reach its environmental goals for CO2?: '
+    # st.subheader(question)
+    # st.subheader(str(prediction))
 
 
-# param1 = st.slider('Select a number', 1, 10, 3)
-
-# param2 = st.slider('Select another number', 1, 10, 3)
-
-# url = 'http://localhost:8080/predict'
-
-# params = {
-#     'feature1': param1,  # 0 for Sunday, 1 for Monday, ...
-#     'feature2': param2
-# }
-# response = requests.get(url, params=params)
-
-# st.text(response.json())
+    st.text(f"{country_selected} will not achieve it's climate goals for 2030")
